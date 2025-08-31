46615fead47949e59174fe1910d786043691b10e
KrishSrini474 <krishsrini.123@gmail.com>
2025-08-31 19:14:18 +0530

    Update ex01_scalar_tensor.py

--- pytorch-tensor-workbook/levels/level01_basics_creation/ex01_scalar_tensor.py
+++ pytorch-tensor-workbook/levels/level01_basics_creation/ex01_scalar_tensor.py
@@ -2,7 +2,7 @@ import torch
 
 def main():
     # TODO: implement ex01_scalar_tensor.py
-    x = torch.tensor(2,0)
+    x = torch.tensor(2.0)
     print("x =",x)
 if __name__ == '__main__':
     main()
